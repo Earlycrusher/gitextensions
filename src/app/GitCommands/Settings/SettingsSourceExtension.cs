a1a0cd926f87a29c5a32d1b788ec96c1e160c52c
Michael Seibt <mstv@gmx.net>
2026-04-25 15:55:48 +0200

    refactor(build server settings): Use ISetting
    implementations StringSetting & BoolSetting
    
    Refs: #13031

--- src/app/GitCommands/Settings/SettingsSourceExtension.cs
+++ src/app/GitCommands/Settings/SettingsSourceExtension.cs
@@ -1,13 +1,9 @@
 ﻿using GitExtensions.Extensibility.Settings;
-using GitUIPluginInterfaces.BuildServerIntegration;
 
 namespace GitCommands.Settings;
 
 public static class SettingsSourceExtension
 {
-    public static IBuildServerSettings GetBuildServerSettings(this SettingsSource settingsSource)
-        => new BuildServerSettings(settingsSource);
-
     public static IDetachedSettings Detached(this SettingsSource settingsSource)
         => new DetachedSettings(settingsSource);
 }
